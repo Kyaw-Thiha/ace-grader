d8ae28653ee450fdf314a2fe829b02c696c22910
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-03-06 01:13:01 +0630

    Added daisy ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ const config = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
 
 module.exports = config;
