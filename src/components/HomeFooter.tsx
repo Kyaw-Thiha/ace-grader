26de3845f8c8501bc0bf7f16458728ce91cd6df8
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-03-07 02:48:49 +0630

    Initialized footer component

---
+++ src/components/HomeFooter.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Footer: NextPage<Props> = () => {
+  return <div></div>;
+};
+
+export default Footer;
+
+{
+  /* <a href="https://www.flaticon.com/free-icons/document" title="document icons">Document icons created by Freepik - Flaticon</a> */
+}
