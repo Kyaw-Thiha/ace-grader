4e070388ec19d939550f8dc69061836771600fa1
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-03-10 01:28:50 +0630

    Created worksheet router

---
+++ src/server/api/routers/worksheet.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "@server/api/trpc";
+
+export const worksheetRouter = createTRPCRouter({
+  get: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.worksheet.findMany({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+
+  getByProfile: protectedProcedure
+    .input(z.object({ profileId: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.worksheet.findMany({
+        where: {
+          profileId: input.profileId,
+        },
+      });
+    }),
+
+  create: protectedProcedure
+    .input(z.object({ title: z.string(), profileId: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.worksheet.create({
+        data: {
+          title: input.title,
+          profileId: input.profileId,
+          status: "PRIVATE",
+          totalMarks: 0,
+        },
+      });
+    }),
+});
