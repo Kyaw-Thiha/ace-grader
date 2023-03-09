86cef27cbee0a3a3d8688f5ea09c34d2a0928e31
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-03-10 01:34:47 +0630

    Added in user, profile, noti and worksheet

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,9 @@
 import { createTRPCRouter } from "@server/api/trpc";
 import { exampleRouter } from "@server/api/routers/example";
+import { teacherProfileRouter } from "./routers/teacherProfile";
+import { userRouter } from "./routers/user";
+import { worksheetRouter } from "./routers/worksheet";
+import { teacherNotificationRouter } from "./routers/teacherNotification";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +12,10 @@ import { exampleRouter } from "@server/api/routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  user: userRouter,
+  teacherProfile: teacherProfileRouter,
+  teacherNotification: teacherNotificationRouter,
+  worksheet: worksheetRouter,
 });
 
 // export type definition of API
