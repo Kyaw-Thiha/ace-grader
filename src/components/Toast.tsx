032152b2b2222431f129bfd3cb02c177e932ca48
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-03-17 02:17:23 +0630

    Created the toast component

---
+++ src/components/Toast.tsx
@@ -0,0 +1,28 @@
+import { Slide, ToastContainer } from "react-toastify";
+import { injectStyle } from "react-toastify/dist/inject-style";
+
+// https://fkhadra.github.io/react-toastify/introduction/
+// https://bobbyhadz.com/blog/react-call-function-in-child-component
+
+const Toast: React.FC = () => {
+  if (typeof window !== "undefined") {
+    injectStyle();
+  }
+
+  return (
+    <ToastContainer
+      position="bottom-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition={Slide}
+    />
+  );
+};
+
+export default Toast;
