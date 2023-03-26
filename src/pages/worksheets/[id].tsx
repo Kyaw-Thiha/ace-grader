b9125ed52c4e82e28dadbed74f22b6b70553a0da
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-03-27 02:33:13 +0630

    Implemented all question types + deletion

--- src/pages/worksheets/[id].tsx
+++ src/pages/worksheets/[id].tsx
@@ -4,10 +4,17 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import { Autosave, useAutosave } from "react-autosave";
-import { api } from "@utils/api";
+import { api, type RouterOutputs } from "@utils/api";
 import Toast from "@components/Toast";
 import AddQuestionButton from "@components/worksheet/AddQuestionButton";
 import Loading from "@components/Loading";
+import MultipleChoiceQuestion from "@components/worksheet/MultipleChoiceQuestion";
+import ShortAnswerQuestion from "@components/worksheet/ShortAnswerQuestion";
+import LongAnswerQuestion from "@components/worksheet/LongAnswerQuestion";
+import Dialog from "@components/Dialog";
+import { type QueryObserverBaseResult } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const WorksheetEditor: NextPage = () => {
   const router = useRouter();
@@ -85,7 +92,6 @@ const WorksheetHeader: React.FC<WorksheetHeaderProps> = ({ worksheetId }) => {
     if (title != "") {
       editTitle.mutate({ id: worksheetId, title: title });
     }
-    return;
   };
   useAutosave({ data: title, onSave: updateTitle });
 
@@ -123,6 +129,9 @@ const QuestionList: React.FC<QuestionListProps> = ({ worksheetId }) => {
   } = api.worksheet.getQuestions.useQuery({ id: worksheetId });
   const questions = worksheet?.questions;
 
+  // Automatically animate the list add and delete
+  const [parent, enableAnimations] = useAutoAnimate(/* optional config */);
+
   if (isLoading) {
     return <Loading />;
   } else if (questions?.length == 0) {
@@ -138,7 +147,7 @@ const QuestionList: React.FC<QuestionListProps> = ({ worksheetId }) => {
         <div className="flex items-center justify-center">
           <AddQuestionButton
             id={worksheetId}
-            order={0}
+            order={1}
             refetch={refetchWorksheet}
           />
         </div>
@@ -146,16 +155,145 @@ const QuestionList: React.FC<QuestionListProps> = ({ worksheetId }) => {
     );
   } else {
     return (
-      <div className="mt-4">
+      <div className="mt-4" ref={parent}>
         {questions?.map((question) => (
           <div
             key={question.id}
-            className="my-4 bg-slate-50 py-4 px-2 md:mx-8 md:rounded-md md:px-4 md:py-8"
+            className="my-4 bg-slate-50 shadow-sm md:mx-8 md:rounded-md"
           >
-            Hi
+            <div className="relative">
+              <label className="absolute right-1 top-1 md:right-2 md:top-2">
+                <DeleteQuestionButton
+                  id={question.id}
+                  order={question.order}
+                  refetch={refetchWorksheet}
+                  questions={questions}
+                />
+              </label>
+            </div>
+            <div className="flex gap-4 py-12 px-2 md:gap-8 md:px-16 md:py-16">
+              <p className="my-2 text-3xl text-slate-400">{question.order}.</p>
+              <div>
+                {question.questionType == "MultipleChoiceQuestion" ? (
+                  <MultipleChoiceQuestion
+                    question={question.multipleChoiceQuestion}
+                    refetch={refetchWorksheet}
+                  />
+                ) : (
+                  <></>
+                )}
+                {question.questionType == "ShortAnswerQuestion" ? (
+                  <ShortAnswerQuestion
+                    question={question.shortAnswerQuestion}
+                    refetch={refetchWorksheet}
+                  />
+                ) : (
+                  <></>
+                )}
+                {question.questionType == "LongAnswerQuestion" ? (
+                  <LongAnswerQuestion
+                    question={question.longAnswerQuestion}
+                    refetch={refetchWorksheet}
+                  />
+                ) : (
+                  <></>
+                )}
+              </div>
+            </div>
           </div>
         ))}
+        <div className="flex h-64 justify-center">
+          <AddQuestionButton
+            id={worksheetId}
+            order={(questions?.length ?? 0) + 1}
+            refetch={refetchWorksheet}
+          />
+        </div>
       </div>
     );
   }
+};
+
+type Questions = RouterOutputs["question"]["getAll"];
+
+interface DeleteQuestionButtonProps {
+  id: string;
+  order: number;
+  questions: Questions;
+  refetch: QueryObserverBaseResult["refetch"];
+}
+
+const DeleteQuestionButton: React.FC<DeleteQuestionButtonProps> = (props) => {
+  const dialogId = `delete-question-${props.order}`;
+
+  //Function for deleting question
+  const deleteQuestion = api.question.delete.useMutation({
+    onSuccess: () => {
+      void props.refetch();
+    },
+  });
+
+  //Function for editing question
+  const editQuestion = api.question.editOrder.useMutation({
+    onSuccess: () => {
+      void props.refetch();
+    },
+  });
+
+  const removeQuestion = () => {
+    void toast
+      .promise(
+        deleteQuestion.mutateAsync({
+          id: props.id,
+        }),
+        {
+          pending: "Removing Question",
+          success: "Question Removed 👌",
+          error: "Error in Question Deletion 🤯",
+        }
+      )
+      .then(() => {
+        for (const question of props.questions) {
+          if (question.order > props.order) {
+            editQuestion.mutate({ id: question.id, order: question.order - 1 });
+          }
+        }
+      });
+  };
+
+  return (
+    <>
+      <Dialog
+        id={dialogId}
+        openContainer={
+          <label htmlFor={dialogId} className="btn-ghost btn text-lg">
+            ✕
+          </label>
+        }
+        body={
+          <>
+            <h3 className="mb-4 text-2xl font-bold">Delete Question</h3>
+            <h4 className="mb-2">Note: This process is irreversible</h4>
+            <p className="mb-6">
+              Are you sure you want to delete this question?
+            </p>
+          </>
+        }
+        actions={
+          <>
+            <label htmlFor={dialogId} className="btn-ghost btn">
+              Cancel
+            </label>
+            <label
+              htmlFor={dialogId}
+              className="btn-warning btn"
+              onClick={removeQuestion}
+            >
+              Delete Worksheet
+            </label>
+          </>
+        }
+      />
+    </>
+  );
 };
