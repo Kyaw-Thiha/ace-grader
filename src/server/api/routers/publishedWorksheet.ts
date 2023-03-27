7e4308c99107f0b260921e08d1cdb53413f01429
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-03-28 02:23:35 +0630

    Implemented creating publishedWorksheet

--- src/server/api/routers/publishedWorksheet.ts
+++ src/server/api/routers/publishedWorksheet.ts
@@ -23,12 +23,104 @@ export const publishedWorksheetRouter = createTRPCRouter({
       });
     }),
 
+  getQuestions: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.publishedWorksheet.findFirst({
+        where: {
+          id: input.id,
+        },
+        select: {
+          questions: {
+            orderBy: {
+              order: "asc",
+            },
+            include: {
+              parentQuestions: {
+                // 1st Level
+                include: {
+                  // 2nd Level
+                  childrenQuestions: {
+                    include: {
+                      // 3rd Level
+                      childrenQuestions: {
+                        include: {
+                          // 4th Level
+                          childrenQuestions: true,
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+              multipleChoiceQuestion: {
+                include: {
+                  choices: true,
+                },
+              },
+              shortAnswerQuestion: true,
+              longAnswerQuestion: true,
+            },
+          },
+        },
+      });
+    }),
+
   create: protectedProcedure
     .input(
       z.object({
         title: z.string(),
+        totalMarks: z.number(),
         profileId: z.string(),
         worksheetId: z.string(),
+        questions: z.array(
+          z.object({
+            order: z.number(),
+            questionType: z.union([
+              z.literal("MultipleChoiceQuestion"),
+              z.literal("ShortAnswerQuestion"),
+              z.literal("LongAnswerQuestion"),
+            ]),
+            multipleChoiceQuestion: z
+              .object({
+                create: z.object({
+                  text: z.string().optional(),
+                  explanation: z.string().optional(),
+                  marks: z.number(),
+                  answer: z.number().optional(),
+                  choices: z.object({
+                    create: z.array(
+                      z.object({
+                        index: z.number(),
+                        text: z.string(),
+                      })
+                    ),
+                  }),
+                }),
+              })
+              .optional(),
+            shortAnswerQuestion: z
+              .object({
+                create: z.object({
+                  text: z.string().optional(),
+                  explanation: z.string().optional(),
+                  marks: z.number(),
+                  answer: z.string().optional(),
+                }),
+              })
+              .optional(),
+            longAnswerQuestion: z
+              .object({
+                create: z.object({
+                  text: z.string().optional(),
+                  explanation: z.string().optional(),
+                  marks: z.number(),
+                  sampleAnswer: z.string().optional(),
+                }),
+              })
+              .optional(),
+          })
+        ),
       })
     )
     .mutation(({ ctx, input }) => {
@@ -36,9 +128,21 @@ export const publishedWorksheetRouter = createTRPCRouter({
         data: {
           title: input.title,
           status: "PRIVATE",
-          totalMarks: 0,
+          totalMarks: input.totalMarks,
           profileId: input.profileId,
           worksheetId: input.worksheetId,
+          questions: {
+            // create: [
+            //   {
+            //     order: 1,
+            //     questionType: "MultipleChoiceQuestion",
+            //     multipleChoiceQuestion: {
+            //       create: { text: "", explanation: "", marks: 0 },
+            //     },
+            //   },
+            // ],
+            create: input.questions,
+          },
         },
       });
     }),
