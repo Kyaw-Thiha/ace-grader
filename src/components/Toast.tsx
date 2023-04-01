c19177017c3dfdc2f2dfc8e0f425d3cb7e6b0799
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-04-01 22:29:14 +0630

    Decreased auto-close time

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -12,7 +12,7 @@ const Toast: React.FC = () => {
   return (
     <ToastContainer
       position="bottom-center"
-      autoClose={5000}
+      autoClose={3000}
       hideProgressBar={false}
       newestOnTop={false}
       rtl={false}
