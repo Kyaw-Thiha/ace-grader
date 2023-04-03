1d4c5da8a42300563b33ae2c74b290966277e75a
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-04-03 22:10:48 +0630

    Created answerSheetRouter

---
+++ src/server/api/routers/answerSheet.ts
@@ -0,0 +1,122 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "@server/api/trpc";
+
+export const answerSheetRouter = createTRPCRouter({
+  get: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.answerSheet.findFirst({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+
+  getQuestions: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.worksheet.findFirst({
+        where: {
+          id: input.id,
+        },
+        select: {
+          questions: {
+            orderBy: {
+              order: "asc",
+            },
+            include: {
+              parentQuestions: {
+                // 1st Level
+                include: {
+                  // 2nd Level
+                  childrenQuestions: {
+                    include: {
+                      // 3rd Level
+                      childrenQuestions: {
+                        include: {
+                          // 4th Level
+                          childrenQuestions: true,
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+              multipleChoiceQuestion: {
+                include: {
+                  choices: true,
+                },
+              },
+              shortAnswerQuestion: true,
+              longAnswerQuestion: true,
+            },
+          },
+        },
+      });
+    }),
+
+  create: protectedProcedure
+    .input(
+      z.object({
+        studentName: z.string(),
+        studentEmail: z.string(),
+        worksheetId: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.answerSheet.create({
+        data: {
+          studentName: input.studentName,
+          studentEmail: input.studentEmail,
+          worksheetId: input.worksheetId,
+        },
+      });
+    }),
+
+  editStudentName: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        studentName: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.answerSheet.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          studentName: input.studentName,
+        },
+      });
+    }),
+
+  editStudentEmail: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        studentEmail: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.answerSheet.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          studentEmail: input.studentEmail,
+        },
+      });
+    }),
+
+  delete: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.answerSheet.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+});
