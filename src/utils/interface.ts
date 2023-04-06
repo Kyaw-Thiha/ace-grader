aa188eee6ebd19b1c983cf646d17f982241f432e
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-04-06 22:31:10 +0630

    Created the file

---
+++ src/utils/interface.ts
@@ -0,0 +1,7 @@
+export type AnswerSheetStatus =
+  | "answering-studentview"
+  | "answering-teacherview"
+  | "checking-studentview"
+  | "checking-teacherview"
+  | "returned-studentview"
+  | "returned-teacherview";
