5043ef227f9fc7bca7691614e04905a70ada1451
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2023-06-27 19:24:52 +0630

    Implemented environmental variable for openai

--- src/server/openai/openai.ts
+++ src/server/openai/openai.ts
@@ -1,7 +1,8 @@
 import { Configuration, OpenAIApi } from "openai";
+import { env } from "@/env.mjs";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
