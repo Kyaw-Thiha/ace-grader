f016493a9e9a2786191aa3103c019effcc8abe57
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2023-06-30 01:20:00 +0630

    Partially implemented the hero section

---
+++ src/pages/index-copy.tsx
@@ -0,0 +1,48 @@
+import { type NextPage } from "next";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import TopNavLayout from "@/components/TopNavLayout";
+
+const Home: NextPage = () => {
+  return (
+    <TopNavLayout>
+      <div className="bg-base-200 min-h-screen">
+        <div className="flex flex-col-reverse gap-8 lg:flex-row">
+          <div className="flex flex-col justify-center">
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6 text-lg">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <div>
+              <SignedOut>
+                <Button asChild>
+                  <Link href="/sign-in">Sign In</Link>
+                </Button>
+              </SignedOut>
+
+              <SignedIn>
+                <Button asChild>
+                  <Link href="/my-worksheets">Go to My Worksheets</Link>
+                </Button>
+              </SignedIn>
+            </div>
+          </div>
+          <Image
+            src="/images/hero.jpg"
+            alt="Hero Image"
+            width="800"
+            height="600"
+            priority
+            className="rounded-lg shadow-2xl"
+          />
+        </div>
+      </div>
+    </TopNavLayout>
+  );
+};
+
+export default Home;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,37 +8,62 @@ import TopNavLayout from "@/components/TopNavLayout";
 const Home: NextPage = () => {
   return (
     <TopNavLayout>
-      <div className="bg-base-200 min-h-screen">
-        <div className="flex flex-col-reverse gap-8 lg:flex-row">
-          <div className="flex flex-col justify-center">
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6 text-lg">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <div>
-              <SignedOut>
-                <Button asChild>
-                  <Link href="/sign-in">Sign In</Link>
-                </Button>
-              </SignedOut>
+      <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
+        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+          <section className="flex flex-col justify-between gap-6 sm:gap-10 md:gap-16 lg:flex-row">
+            <div className="flex flex-col justify-center sm:text-center lg:py-12 lg:text-left xl:w-5/12 xl:py-24">
+              <p className="mb-4 font-semibold text-indigo-500 md:mb-6 md:text-lg xl:text-xl">
+                Very proud to introduce
+              </p>
 
-              <SignedIn>
-                <Button asChild>
-                  <Link href="/my-worksheets">Go to My Worksheets</Link>
-                </Button>
-              </SignedIn>
+              <h1 className="text-black-800 mb-8 text-4xl font-bold sm:text-5xl md:mb-12 md:text-6xl">
+                Revolutionary way to grade papers
+              </h1>
+
+              <p className="mb-8 leading-relaxed text-gray-500 md:mb-12 lg:w-4/5 xl:text-lg">
+                This is a section of some simple filler text, also known as
+                placeholder text. It shares some characteristics of a real
+                written text but is random.
+              </p>
+
+              <div className="flex flex-col gap-2.5 sm:flex-row sm:justify-center lg:justify-start">
+                {/* <SignedOut>
+                  <Button asChild>
+                    <Link href="/sign-in">Sign In</Link>
+                  </Button>
+                </SignedOut>
+
+                <SignedIn>
+                  <Button asChild>
+                    <Link href="/my-worksheets">Go to My Worksheets</Link>
+                  </Button>
+                </SignedIn> */}
+                <a
+                  href="#"
+                  className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"
+                >
+                  Start now
+                </a>
+
+                <a
+                  href="#"
+                  className="inline-block rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base"
+                >
+                  Take tour
+                </a>
+              </div>
             </div>
-          </div>
-          <Image
-            src="/images/hero.jpg"
-            alt="Hero Image"
-            width="800"
-            height="600"
-            priority
-            className="rounded-lg shadow-2xl"
-          />
+
+            <div className="h-48 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:h-auto xl:w-5/12">
+              <Image
+                src="/images/hero.jpg"
+                width="1000"
+                height="500"
+                alt="Hero Image"
+                className="h-full w-full object-cover object-center"
+              />
+            </div>
+          </section>
         </div>
       </div>
     </TopNavLayout>
