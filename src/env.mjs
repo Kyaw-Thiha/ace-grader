5cd43556178146dc4d03d9246cf03070bbcafe1d
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2023-07-21 22:41:56 +0630

    Installed and configured uploadthing

--- src/env.mjs
+++ src/env.mjs
@@ -11,6 +11,7 @@ export const env = createEnv({
     CLERK_SECRET_KEY: z.string(),
     UPSTASH_REDIS_REST_URL: z.string(),
     UPSTASH_REDIS_REST_TOKEN: z.string(),
+    UPLOADTHING_SECRET: z.string(),
     NODE_ENV: z.enum(["development", "test", "production"]),
   },
 
@@ -48,6 +49,7 @@ export const env = createEnv({
     NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
     UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
     UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+    UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
