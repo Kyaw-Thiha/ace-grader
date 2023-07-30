c57539905ddf9b8eb02c73ef4fcaf64c8662c90b
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2023-07-31 03:14:53 +0630

    Added formatDate function

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -41,3 +41,52 @@ export const convertASCIIToInteger = (string: string) => {
   }
   return string.charCodeAt(0) - 64;
 };
+
+/**
+ * Convert Date to a better format
+ * Eg: 1st Jun 2004
+ *
+ * @param Date
+ * @returns formattedDate
+ */
+export const formatDate = (date: Date) => {
+  const day = date.getDate();
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const month = monthNames[date.getMonth()];
+  const year = date.getFullYear();
+
+  let dayWithSuffix = day.toString();
+  if (day >= 11 && day <= 13) {
+    dayWithSuffix += "th";
+  } else {
+    const lastDigit = day % 10;
+    switch (lastDigit) {
+      case 1:
+        dayWithSuffix += "st";
+        break;
+      case 2:
+        dayWithSuffix += "nd";
+        break;
+      case 3:
+        dayWithSuffix += "rd";
+        break;
+      default:
+        dayWithSuffix += "th";
+    }
+  }
+
+  return `${dayWithSuffix} ${month ?? ""} ${year}`;
+};
