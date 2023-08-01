9bb6697d40c80ce0f94e314336fcec8d21132f0b
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2023-08-01 18:37:36 +0630

    Implemented features section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { type NextPage } from "next";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import TopNavLayout from "@/components/TopNavLayout";
+import { cn } from "@/lib/utils";
+
+import HeroImage from "public/images/hero.jpg";
+import EditWorksheetImage from "public/images/features/edit-worksheet.png";
+import GenerateExplanationImage from "public/images/features/generate-explanation.png";
+import ShareWorksheetImage from "public/images/features/share-worksheet.png";
+import AnswerWorksheetImage from "public/images/features/answer-worksheet.png";
+import CheckWorksheetImage from "public/images/features/check-worksheet.png";
 
 const Home: NextPage = () => {
   return (
@@ -66,9 +75,10 @@ const HeroSection = () => {
 
           <div className="h-48 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:h-auto xl:w-5/12">
             <Image
-              src="/images/hero.jpg"
-              width="1000"
-              height="500"
+              priority
+              src={HeroImage}
+              // width="1000"
+              // height="500"
               alt="Hero Image"
               className="h-full w-full object-cover object-center"
             />
@@ -79,118 +89,181 @@ const HeroSection = () => {
   );
 };
 
+export default Home;
+
 const FeatureSection = () => {
+  const features = [
+    {
+      text: "Create and edit your worksheet",
+      image: EditWorksheetImage,
+    },
+    {
+      text: "Automatically generate explanation using AI",
+      image: GenerateExplanationImage,
+    },
+    {
+      text: "Publish and share your worksheet",
+      image: ShareWorksheetImage,
+    },
+    {
+      text: "The students will answer the worksheet",
+      image: AnswerWorksheetImage,
+    },
+    {
+      text: "Automatically grade the worksheets and give personalized feedback using AI",
+      image: CheckWorksheetImage,
+    },
+  ];
+
+  const [currentFeature, setCurrentFeature] = useState(features[0]);
+
   return (
-    <section className=" text-gray-600">
-      <div className="container mx-auto px-5 py-24">
-        <h1 className="mb-20 text-center text-2xl font-medium text-gray-900 sm:text-3xl">
-          Features
-        </h1>
-        <div className="-mx-4 -mb-10 -mt-4 flex flex-wrap space-y-6 sm:-m-4 md:space-y-0">
-          <div className="flex p-4 md:w-1/3">
-            <div className="mb-4 inline-flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-indigo-100 text-indigo-500">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="h-6 w-6"
-                viewBox="0 0 24 24"
+    <section>
+      <h1 className="mb-8 text-center text-2xl font-medium text-gray-900 sm:text-3xl">
+        Features
+      </h1>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="flex flex-col items-center justify-center gap-4">
+          {features.map((feature, index) => {
+            return (
+              <div
+                key={feature.text}
+                className={cn(
+                  "flex w-full cursor-pointer justify-start rounded-lg px-4 py-4 transition-all hover:bg-accent hover:text-accent-foreground active:bg-blue-100 dark:active:bg-sky-950/60",
+                  feature.text == currentFeature?.text && "bg-accent"
+                )}
+                onClick={() => {
+                  setCurrentFeature(feature);
+                }}
               >
-                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-              </svg>
-            </div>
-            <div className="flex-grow pl-6">
-              <h2 className="title-font mb-2 text-lg font-medium text-gray-900">
-                Effortless Worksheet Sharing
-              </h2>
-              <p className="text-base leading-relaxed">
-                Creating a worksheet is as easy as a few clicks. Once you&#39;ve
-                crafted your custom worksheet, effortlessly share it with your
-                students using our shareable link feature. Gone are the days of
-                printing stacks of papers or dealing with lost assignments.
-                AceGrader makes sharing and distributing worksheets a breeze.
-              </p>
-              {/* <a className="mt-3 inline-flex items-center text-indigo-500">
-                Learn More
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="ml-2 h-4 w-4"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a> */}
-            </div>
-          </div>
-          <div className="flex p-4 md:w-1/3">
-            <div className="mb-4 inline-flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-indigo-100 text-indigo-500">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="h-6 w-6"
-                viewBox="0 0 24 24"
-              >
-                <circle cx="6" cy="6" r="3"></circle>
-                <circle cx="6" cy="18" r="3"></circle>
-                <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-              </svg>
-            </div>
-            <div className="flex-grow pl-6">
-              <h2 className="title-font mb-2 text-lg font-medium text-gray-900">
-                Seamless Student Engagement
-              </h2>
-              <p className="text-base leading-relaxed">
-                Students can access their assigned worksheets from any device
-                with an internet connection. Our user-friendly interface allows
-                them to answer questions directly on the platform, making the
-                learning process more interactive and engaging. Say goodbye to
-                traditional pen and paper, and embrace the digital age of
-                education.
-              </p>
-            </div>
-          </div>
-          <div className="flex p-4 md:w-1/3">
-            <div className="mb-4 inline-flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-indigo-100 text-indigo-500">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="h-6 w-6"
-                viewBox="0 0 24 24"
-              >
-                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                <circle cx="12" cy="7" r="4"></circle>
-              </svg>
-            </div>
-            <div className="flex-grow pl-6">
-              <h2 className="title-font mb-2 text-lg font-medium text-gray-900">
-                Automated Grading Made Simple
-              </h2>
-              <p className="text-base leading-relaxed">
-                AceGrader takes the hassle out of grading. By configuring your
-                desired marking scheme, our intelligent system automatically
-                checks student answers against the preset criteria. The days of
-                manually checking every response are over, allowing you to focus
-                on providing valuable feedback and personalized attention to
-                your students.
-              </p>
-            </div>
-          </div>
+                <span className="mr-2 align-top">{index + 1}.</span>
+                {feature.text}
+              </div>
+            );
+          })}
+        </div>
+        <div className="col-span-2 flex items-center justify-center rounded-md border px-8 py-8">
+          <Image
+            className="max-h-96 w-auto"
+            src={currentFeature?.image ?? EditWorksheetImage}
+            alt={currentFeature?.text ?? ""}
+          />
         </div>
       </div>
     </section>
   );
 };
 
-export default Home;
+// const FeatureSection = () => {
+//   return (
+//     <section className=" text-gray-600">
+//       <div className="container mx-auto px-5 py-24">
+//         <h1 className="mb-20 text-center text-2xl font-medium text-gray-900 sm:text-3xl">
+//           Features
+//         </h1>
+//         <div className="-mx-4 -mb-10 -mt-4 flex flex-wrap space-y-6 sm:-m-4 md:space-y-0">
+//           <div className="flex p-4 md:w-1/3">
+//             <div className="mb-4 inline-flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-indigo-100 text-indigo-500">
+//               <svg
+//                 fill="none"
+//                 stroke="currentColor"
+//                 strokeLinecap="round"
+//                 strokeLinejoin="round"
+//                 strokeWidth="2"
+//                 className="h-6 w-6"
+//                 viewBox="0 0 24 24"
+//               >
+//                 <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+//               </svg>
+//             </div>
+//             <div className="flex-grow pl-6">
+//               <h2 className="title-font mb-2 text-lg font-medium text-gray-900">
+//                 Effortless Worksheet Sharing
+//               </h2>
+//               <p className="text-base leading-relaxed">
+//                 Creating a worksheet is as easy as a few clicks. Once you&#39;ve
+//                 crafted your custom worksheet, effortlessly share it with your
+//                 students using our shareable link feature. Gone are the days of
+//                 printing stacks of papers or dealing with lost assignments.
+//                 AceGrader makes sharing and distributing worksheets a breeze.
+//               </p>
+//               {/* <a className="mt-3 inline-flex items-center text-indigo-500">
+//                 Learn More
+//                 <svg
+//                   fill="none"
+//                   stroke="currentColor"
+//                   stroke-linecap="round"
+//                   stroke-linejoin="round"
+//                   stroke-width="2"
+//                   className="ml-2 h-4 w-4"
+//                   viewBox="0 0 24 24"
+//                 >
+//                   <path d="M5 12h14M12 5l7 7-7 7"></path>
+//                 </svg>
+//               </a> */}
+//             </div>
+//           </div>
+//           <div className="flex p-4 md:w-1/3">
+//             <div className="mb-4 inline-flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-indigo-100 text-indigo-500">
+//               <svg
+//                 fill="none"
+//                 stroke="currentColor"
+//                 strokeLinecap="round"
+//                 strokeLinejoin="round"
+//                 strokeWidth="2"
+//                 className="h-6 w-6"
+//                 viewBox="0 0 24 24"
+//               >
+//                 <circle cx="6" cy="6" r="3"></circle>
+//                 <circle cx="6" cy="18" r="3"></circle>
+//                 <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+//               </svg>
+//             </div>
+//             <div className="flex-grow pl-6">
+//               <h2 className="title-font mb-2 text-lg font-medium text-gray-900">
+//                 Seamless Student Engagement
+//               </h2>
+//               <p className="text-base leading-relaxed">
+//                 Students can access their assigned worksheets from any device
+//                 with an internet connection. Our user-friendly interface allows
+//                 them to answer questions directly on the platform, making the
+//                 learning process more interactive and engaging. Say goodbye to
+//                 traditional pen and paper, and embrace the digital age of
+//                 education.
+//               </p>
+//             </div>
+//           </div>
+//           <div className="flex p-4 md:w-1/3">
+//             <div className="mb-4 inline-flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-indigo-100 text-indigo-500">
+//               <svg
+//                 fill="none"
+//                 stroke="currentColor"
+//                 strokeLinecap="round"
+//                 strokeLinejoin="round"
+//                 strokeWidth="2"
+//                 className="h-6 w-6"
+//                 viewBox="0 0 24 24"
+//               >
+//                 <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+//                 <circle cx="12" cy="7" r="4"></circle>
+//               </svg>
+//             </div>
+//             <div className="flex-grow pl-6">
+//               <h2 className="title-font mb-2 text-lg font-medium text-gray-900">
+//                 Automated Grading Made Simple
+//               </h2>
+//               <p className="text-base leading-relaxed">
+//                 AceGrader takes the hassle out of grading. By configuring your
+//                 desired marking scheme, our intelligent system automatically
+//                 checks student answers against the preset criteria. The days of
+//                 manually checking every response are over, allowing you to focus
+//                 on providing valuable feedback and personalized attention to
+//                 your students.
+//               </p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
