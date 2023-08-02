230948b7c8791ed50ddeec83834538279989dd97
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2023-08-02 23:35:20 +0630

    Implemented sitemap

---
+++ next-sitemap.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('next-sitemap').IConfig} */
+
+const config = {
+  siteUrl: "https://acegrader.com",
+  generateRobotsTxt: true,
+  exclude: ["/worksheets", "/published-worksheets"],
+  additionalPaths: async (config) => {
+    const result = [];
+
+    result.push(
+      await config.transform(config, "/sign-up"),
+      await config.transform(config, "/sign-in")
+    );
+
+    return result;
+  },
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/worksheets", "/published-worksheets"],
+      },
+    ],
+  },
+};
+
+export default config;
