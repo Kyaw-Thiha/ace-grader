3909734a851b94a7a070469f6e86ce7e35cbc215
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2023-08-08 11:11:50 +0630

    Changed question font size

--- src/components/answerSheet/OpenEndedQuestion.tsx
+++ src/components/answerSheet/OpenEndedQuestion.tsx
@@ -42,7 +42,7 @@ const OpenEndedQuestion: React.FC<Props> = (props) => {
   return (
     <div className="flex flex-col">
       <CardHeader>
-        <CardTitle className="whitespace-pre-line font-normal leading-relaxed">
+        <CardTitle className="whitespace-pre-line text-lg font-normal leading-relaxed">
           <Latex
             delimiters={[
               { left: "$$", right: "$$", display: true },
