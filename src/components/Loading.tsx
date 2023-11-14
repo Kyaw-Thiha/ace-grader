7f14894b68777b845ed99ab9197292cfc8514985
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2023-11-14 20:47:24 +0630

    Updated the packages

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,9 @@
-import Lottie from "lottie-react";
+// import Lottie from "lottie-react";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
 import loadingPlane from "public/lotties/loading-plane.json";
 import loadingFile from "public/lotties/loading-files.json";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { useEffect, useState } from "react";
 
 // https://lottiereact.com/
 // https://lottiefiles.com/99297-loading-files
@@ -9,17 +11,36 @@ import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const Loading: React.FC = () => {
   const [parent, enableAnimations] = useAutoAnimate(/* optional config */);
+  // if (typeof window !== "undefined") {
+  //   injectStyle();
+  // }
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return null;
+  }
 
   const style = {
     height: 500,
   };
 
-  return (
-    <div className="flex flex-col items-center justify-center" ref={parent}>
-      <Lottie animationData={loadingPlane as unknown} style={style} />
-      <h3 className="text-3xl"> Getting your data ...</h3>
-    </div>
-  );
+  if (typeof document !== "undefined") {
+    return (
+      <div className="flex flex-col items-center justify-center" ref={parent}>
+        {/* <Lottie animationData={loadingPlane as unknown} style={style} /> */}
+        {/* <Player autoplay loop src={loadingPlane} style={style}>
+          <Controls
+            visible={false}
+            buttons={["play", "repeat", "frame", "debug"]}
+          />
+        </Player> */}
+        <h3 className="text-3xl"> Getting your data ...</h3>
+      </div>
+    );
+  }
 };
 
 export default Loading;
